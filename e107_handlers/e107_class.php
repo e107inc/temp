9d752479fc14d3d431f83c39c9bc274eda62f351
camer0n <e107inc@gmail.com>
2023-11-13 11:55:03 -0800

    Dump headers

--- e107_handlers/e107_class.php
+++ e107_handlers/e107_class.php
@@ -4745,15 +4745,15 @@ class e107
 	//	self::ini_set('arg_separator.output',     '&amp;'); // non-standard and bad for third-party script compatibility. @see https://github.com/e107inc/e107/issues/3116
 //		self::ini_set('session.use_only_cookies', 1);
 
-if(headers_sent())
-{
+
 	$headers = headers_list();
+	echo "\n--- Headers ---\n";
 	foreach ($headers as $header)
 	{
 	    echo $header . "\n";
 	}
 
-}
+
 
 		self::ini_set('session.use_trans_sid',    0);
 		self::ini_set('session.cookie_httponly',  1); // cookie won't be accessible by scripting languages, such as JavaScript. Can effectively help to reduce identity theft through XSS attacks
